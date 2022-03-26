1c9f3e904e430f6545eef957f73e9b2a80f21d05
Ayoub Nait Lamine <aynaitlamine.dev@gmail.com>
2022-03-26 12:03:59 +0100

    add story features

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image } from "react-native";
+import { StyleSheet, View, Image, FlatList } from "react-native";
 import { Text } from "react-native-paper";
-import { LinearGradient } from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
 import { Provider } from "react-native-paper";
 import {
   CameraIcon,
@@ -9,7 +9,14 @@ import {
   InstagramIcon,
   MessengerIcon,
   OvalIcon,
+  MoreIcon,
+  VerfiedIcon,
+  LikeIcon,
+  CommentIcon,
+  SaveIcon,
+  PaginationIcon,
 } from "./src/assets/icons";
+import { borderTopColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
 
 export default function App() {
   return (
@@ -25,12 +32,191 @@ export default function App() {
           </View>
         </View>
         <View style={styles.stories}>
-        <LinearGradient
-  colors={['#CA1D7E', '#E35157', '#F2703F']}
-  start={{x: 0.0, y: 1.0}} end={{x: 1.0, y: 1.0}}
-  style={{ height: 82,width: 82,alignItems: 'center',justifyContent: 'center',borderRadius:82 / 2}}>
-          <Image resizeMode="cover" style={{width: 77,height: 77,borderRadius: 77/2,alignSelf: 'center',borderColor: '#fff',borderWidth: 2}} source={ require('./src/assets/images/profile.jpg')} />
-          </LinearGradient>
+          <FlatList
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+            data={[1]}
+            renderItem={({ item }) => (
+              <>
+                <View style={styles.story}>
+                  <LinearGradient
+                    colors={["#CA1D7E", "#E35157", "#F2703F"]}
+                    start={{ x: 0.0, y: 1.0 }}
+                    end={{ x: 1.0, y: 1.0 }}
+                    style={styles.storyGradient}
+                  >
+                    <Image
+                      resizeMode="cover"
+                      style={{
+                        width: 58,
+                        height: 58,
+                        borderRadius: 58 / 2,
+                        alignSelf: "center",
+                        borderColor: "#fff",
+                        borderWidth: 2,
+                      }}
+                      source={require("./src/assets/images/profile-1.jpg")}
+                    />
+                  </LinearGradient>
+                  <Text style={styles.storyText}>Your story</Text>
+                </View>
+                <View style={styles.story}>
+                  <LinearGradient
+                    colors={["#CA1D7E", "#E35157", "#F2703F"]}
+                    start={{ x: 0.0, y: 1.0 }}
+                    end={{ x: 1.0, y: 1.0 }}
+                    style={styles.storyGradient}
+                  >
+                    <Image
+                      resizeMode="cover"
+                      style={{
+                        width: 58,
+                        height: 58,
+                        borderRadius: 58 / 2,
+                        alignSelf: "center",
+                        borderColor: "#fff",
+                        borderWidth: 2,
+                      }}
+                      source={require("./src/assets/images/profile-2.jpg")}
+                    />
+                  </LinearGradient>
+                  <LinearGradient
+                    colors={["#E35157", "#CA1D7E"]}
+                    start={{ x: 1, y: 0 }}
+                    end={{ x: 0, y: 0 }}
+                    style={styles.storyLive}
+                  >
+                    <Text style={styles.storyLiveText}>LIVE</Text>
+                  </LinearGradient>
+                  <Text style={styles.storyText}>karennne</Text>
+                </View>
+                <View style={styles.story}>
+                  <LinearGradient
+                    colors={["#CA1D7E", "#E35157", "#F2703F"]}
+                    start={{ x: 0.0, y: 1.0 }}
+                    end={{ x: 1.0, y: 1.0 }}
+                    style={styles.storyGradient}
+                  >
+                    <Image
+                      resizeMode="cover"
+                      style={{
+                        width: 58,
+                        height: 58,
+                        borderRadius: 58 / 2,
+                        alignSelf: "center",
+                        borderColor: "#fff",
+                        borderWidth: 2,
+                      }}
+                      source={require("./src/assets/images/profile-3.jpg")}
+                    />
+                  </LinearGradient>
+                  <Text style={styles.storyText}>zackjohn</Text>
+                </View>
+                <View style={styles.story}>
+                  <LinearGradient
+                    colors={["#CA1D7E", "#E35157", "#F2703F"]}
+                    start={{ x: 0.0, y: 1.0 }}
+                    end={{ x: 1.0, y: 1.0 }}
+                    style={styles.storyGradient}
+                  >
+                    <Image
+                      resizeMode="cover"
+                      style={{
+                        width: 58,
+                        height: 58,
+                        borderRadius: 58 / 2,
+                        alignSelf: "center",
+                        borderColor: "#fff",
+                        borderWidth: 2,
+                      }}
+                      source={require("./src/assets/images/profile-4.jpg")}
+                    />
+                  </LinearGradient>
+                  <Text style={styles.storyText}>craig_love</Text>
+                </View>
+                <View style={styles.story}>
+                  <LinearGradient
+                    colors={["#CA1D7E", "#E35157", "#F2703F"]}
+                    start={{ x: 0.0, y: 1.0 }}
+                    end={{ x: 1.0, y: 1.0 }}
+                    style={styles.storyGradient}
+                  >
+                    <Image
+                      resizeMode="cover"
+                      style={{
+                        width: 58,
+                        height: 58,
+                        borderRadius: 58 / 2,
+                        alignSelf: "center",
+                        borderColor: "#fff",
+                        borderWidth: 2,
+                      }}
+                      source={require("./src/assets/images/profile-5.jpg")}
+                    />
+                  </LinearGradient>
+
+                  <Text style={styles.storyText}>kieron_d</Text>
+                </View>
+              </>
+            )}
+          />
+        </View>
+        <View style={styles.postContainer}>
+          <View style={styles.postHeader}>
+            <View style={styles.postUserInfo}>
+              <Image
+                resizeMode="cover"
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: 500,
+                }}
+                source={require("./src/assets/images/profile-2.jpg")}
+              />
+              <View style={styles.postUserName}>
+                <View style={styles.postVerfied}>
+                  <Text
+                    style={{
+                      fontSize: 14,
+                      fontWeight: "bold",
+                      paddingRight: 4,
+                    }}
+                  >
+                    karennne
+                  </Text>
+                  <VerfiedIcon style={styles.verfiedIcon} />
+                </View>
+                <Text style={{ fontSize: 10 }}>New York, United State</Text>
+              </View>
+            </View>
+            <MoreIcon style={styles.postHeadermoreIcon} />
+          </View>
+          <View style={styles.postImage}>
+            <Image
+              style={{ width: "100%", height: "100%" }}
+              resizeMode="cover"
+              source={require("./src/assets/images/post_image.jpg")}
+            />
+          </View>
+          <View style={styles.postReactionContainer}>
+            <View style={styles.postReaction}>
+              <View style={{ marginLeft: 8 }}>
+                <LikeIcon />
+              </View>
+              <View style={{ marginLeft: 8 }}>
+                <CommentIcon />
+              </View>
+              <View style={{ marginLeft: 8 }}>
+                <MessengerIcon />
+              </View>
+            </View>
+            <View style={{ position: "absolute", top: 0, right: 0 }}>
+              <SaveIcon />
+            </View>
+            <View>
+              <PaginationIcon />
+            </View>
+          </View>
         </View>
         <StatusBar style="auto" />
       </View>
@@ -51,7 +237,7 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignContent: "center",
     borderBottomWidth: 0.6,
-    borderBottomColor: "#A6A6AA",
+    borderBottomColor: "#DDD",
   },
   row: {
     flexDirection: "row",
@@ -81,6 +267,90 @@ const styles = StyleSheet.create({
     right: 49,
   },
   stories: {
+    flexDirection: "row",
+    alignItems: "flex-start",
+    marginVertical: 10,
+  },
+  story: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 10,
+  },
+  storyGradient: {
+    position: "relative",
+    height: 62,
+    width: 62,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 62 / 2,
+    marginBottom: 5,
+  },
+  storyText: {
+    fontSize: 12,
+  },
 
-  }
+  storyLive: {
+    position: "absolute",
+    bottom: 14,
+    paddingHorizontal: 4,
+    paddingVertical: 2,
+    borderWidth: 2,
+    borderColor: "white",
+    borderRadius: 5,
+  },
+  storyLiveText: {
+    color: "white",
+    fontSize: 8,
+  },
+  postContainer: {
+    marginVertical: 10,
+    borderTopColor: "#DDD",
+    borderTopWidth: 0.6,
+  },
+  postHeader: {
+    position: "relative",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingVertical: 8,
+    paddingHorizontal: 10,
+  },
+  postUserInfo: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  postUserName: {
+    paddingHorizontal: 10,
+  },
+  postVerfied: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+
+  postHeadermoreIcon: {
+    position: "absolute",
+    top: 0,
+    left: 10,
+  },
+  postImage: {
+    width: "100%",
+    height: 300,
+  },
+  postReactionContainer: {
+
+
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 10,
+  },
+  postReaction: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -89,3 +89,112 @@ export const OvalIcon = ({style}) => (
       ></Path>
     </Svg>
 )
+
+export const VerfiedIcon = () => (
+  <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="10"
+      height="10"
+      fill="none"
+      viewBox="0 0 10 10"
+    >
+      <Path
+        fill="#3897F0"
+        fillRule="evenodd"
+        d="M4.95 0L6.18.803l1.471-.01.603 1.343 1.242.787-.219 1.455.62 1.334-.97 1.105-.2 1.457-1.414.405-.956 1.118-1.409-.423-1.409.423-.956-1.118-1.414-.405-.2-1.457L0 5.712l.62-1.334L.4 2.923l1.244-.787.602-1.342 1.47.01L4.95 0zm1.308 3.519l-1.969 2.09-.649-.69a.4.4 0 00-.582.55l.94.998a.4.4 0 00.582 0l2.26-2.4a.4.4 0 00-.582-.548z"
+        clipRule="evenodd"
+      ></Path>
+    </Svg>
+)
+
+export const MoreIcon = () => (
+  <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="14"
+      height="4"
+      fill="none"
+      viewBox="0 0 14 4"
+    >
+      <Path
+        fill="#262626"
+        fillRule="evenodd"
+        d="M1.5.5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zM7 .5a1.5 1.5 0 110 3 1.5 1.5 0 010-3zm5.5 0a1.5 1.5 0 110 3 1.5 1.5 0 010-3z"
+        clipRule="evenodd"
+      ></Path>
+    </Svg>
+)
+
+export const CommentIcon = () => (
+  <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="23"
+      fill="none"
+      viewBox="0 0 22 23"
+    >
+      <Path
+        fill="#262626"
+        fillRule="evenodd"
+        d="M22 11.5c0-6.075-4.925-11-11-11S0 5.425 0 11.5s4.925 11 11 11c1.326 0 3.148-.484 5.465-1.451l3.746 1.461.111.037a1 1 0 001.224-1.204l-1.044-4.297.17-.41C21.559 14.482 22 12.77 22 11.5zm-2.729 4.6l-.345.817.936 3.847-3.417-1.333-.933.387C13.544 20.612 12.024 21 11 21a9.5 9.5 0 119.5-9.5c0 1.046-.403 2.595-1.229 4.6z"
+        clipRule="evenodd"
+      ></Path>
+    </Svg>
+)
+
+export const LikeIcon = () => (
+  <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="22"
+      fill="none"
+      viewBox="0 0 24 22"
+    >
+      <Path
+        fill="#262626"
+        fillRule="evenodd"
+        d="M11.873 3.159l-.655-.766c-2.637-2.636-6.84-2.456-9.476.18-2.636 2.636-2.13 7.732.507 10.368 1.07 1.07 2.672 2.551 4.806 4.444l1.796 1.58 2.017 1.75a1.5 1.5 0 001.94.018l1.634-1.37c2.86-2.418 4.984-4.323 6.37-5.714l.359-.366.323-.342c2.455-2.647 3.086-7.793.511-10.368l-.186-.18c-2.648-2.455-6.64-2.574-9.215 0l-.731.766zm-9.07.475c2.084-2.085 5.308-2.183 7.315-.218l1.693 1.978L13.69 3.43c1.925-1.926 4.995-1.898 7.11.063l.166.16c1.819 1.82 1.51 6.025-.571 8.268l-.313.33-.346.354c-1.149 1.152-2.85 2.697-5.095 4.62l-1.167.992-1.625 1.363-2.014-1.748-1.44-1.266c-2.032-1.795-3.598-3.23-4.695-4.3l-.39-.385C1.139 9.709.867 5.569 2.803 3.634z"
+        clipRule="evenodd"
+      ></Path>
+    </Svg>
+)
+
+export const SaveIcon = () => (
+  <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="22"
+      height="24"
+      fill="none"
+      viewBox="0 0 22 24"
+    >
+      <Path
+        fill="#262626"
+        fillRule="evenodd"
+        d="M.86 1.179V22.83a1 1 0 001.703.712l8.548-8.43 8.549 8.43a1 1 0 001.702-.712V1.179a.5.5 0 00-.5-.5H1.361a.5.5 0 00-.5.5zm1.5 20.457V2.179h17.502v19.457L11.813 13.7a1 1 0 00-1.404 0l-8.048 7.936z"
+        clipRule="evenodd"
+      ></Path>
+    </Svg>
+)
+
+export const PaginationIcon = () => (
+  <Svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="27"
+      height="6"
+      fill="none"
+      viewBox="0 0 27 6"
+    >
+      <Path
+        fill="#3897F0"
+        fillRule="evenodd"
+        d="M3.5 6a3 3 0 100-6 3 3 0 000 6z"
+        clipRule="evenodd"
+      ></Path>
+      <Path
+        fill="#000"
+        fillOpacity="0.15"
+        fillRule="evenodd"
+        d="M13.5 6a3 3 0 100-6 3 3 0 000 6zM23.5 6a3 3 0 100-6 3 3 0 000 6z"
+        clipRule="evenodd"
+      ></Path>
+    </Svg>
+)
