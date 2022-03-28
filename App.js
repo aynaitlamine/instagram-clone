6e343d8b03a222e91287ee0efd61a59856ff8af5
Ayoub Nait Lamine <aynaitlamine.dev@gmail.com>
2022-03-28 09:22:52 +0000

    Adding instagram post

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image, FlatList } from "react-native";
-import { Text } from "react-native-paper";
-import { LinearGradient } from "expo-linear-gradient";
-import { Provider } from "react-native-paper";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Image, FlatList } from 'react-native';
+import { Text, Paragraph } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Provider } from 'react-native-paper';
 import {
   CameraIcon,
   IgtvIcon,
@@ -15,8 +15,8 @@ import {
   CommentIcon,
   SaveIcon,
   PaginationIcon,
-} from "./src/assets/icons";
-import { borderTopColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
+} from './src/assets/icons';
+import { borderTopColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 export default function App() {
   return (
@@ -40,48 +40,48 @@ export default function App() {
               <>
                 <View style={styles.story}>
                   <LinearGradient
-                    colors={["#CA1D7E", "#E35157", "#F2703F"]}
+                    colors={['#CA1D7E', '#E35157', '#F2703F']}
                     start={{ x: 0.0, y: 1.0 }}
                     end={{ x: 1.0, y: 1.0 }}
                     style={styles.storyGradient}
                   >
                     <Image
-                      resizeMode="cover"
+                      resizeMode='cover'
                       style={{
                         width: 58,
                         height: 58,
                         borderRadius: 58 / 2,
-                        alignSelf: "center",
-                        borderColor: "#fff",
+                        alignSelf: 'center',
+                        borderColor: '#fff',
                         borderWidth: 2,
                       }}
-                      source={require("./src/assets/images/profile-1.jpg")}
+                      source={require('./src/assets/images/profile-1.jpg')}
                     />
                   </LinearGradient>
                   <Text style={styles.storyText}>Your story</Text>
                 </View>
                 <View style={styles.story}>
                   <LinearGradient
-                    colors={["#CA1D7E", "#E35157", "#F2703F"]}
+                    colors={['#CA1D7E', '#E35157', '#F2703F']}
                     start={{ x: 0.0, y: 1.0 }}
                     end={{ x: 1.0, y: 1.0 }}
                     style={styles.storyGradient}
                   >
                     <Image
-                      resizeMode="cover"
+                      resizeMode='cover'
                       style={{
                         width: 58,
                         height: 58,
                         borderRadius: 58 / 2,
-                        alignSelf: "center",
-                        borderColor: "#fff",
+                        alignSelf: 'center',
+                        borderColor: '#fff',
                         borderWidth: 2,
                       }}
-                      source={require("./src/assets/images/profile-2.jpg")}
+                      source={require('./src/assets/images/profile-2.jpg')}
                     />
                   </LinearGradient>
                   <LinearGradient
-                    colors={["#E35157", "#CA1D7E"]}
+                    colors={['#E35157', '#CA1D7E']}
                     start={{ x: 1, y: 0 }}
                     end={{ x: 0, y: 0 }}
                     style={styles.storyLive}
@@ -92,66 +92,66 @@ export default function App() {
                 </View>
                 <View style={styles.story}>
                   <LinearGradient
-                    colors={["#CA1D7E", "#E35157", "#F2703F"]}
+                    colors={['#CA1D7E', '#E35157', '#F2703F']}
                     start={{ x: 0.0, y: 1.0 }}
                     end={{ x: 1.0, y: 1.0 }}
                     style={styles.storyGradient}
                   >
                     <Image
-                      resizeMode="cover"
+                      resizeMode='cover'
                       style={{
                         width: 58,
                         height: 58,
                         borderRadius: 58 / 2,
-                        alignSelf: "center",
-                        borderColor: "#fff",
+                        alignSelf: 'center',
+                        borderColor: '#fff',
                         borderWidth: 2,
                       }}
-                      source={require("./src/assets/images/profile-3.jpg")}
+                      source={require('./src/assets/images/profile-3.jpg')}
                     />
                   </LinearGradient>
                   <Text style={styles.storyText}>zackjohn</Text>
                 </View>
                 <View style={styles.story}>
                   <LinearGradient
-                    colors={["#CA1D7E", "#E35157", "#F2703F"]}
+                    colors={['#CA1D7E', '#E35157', '#F2703F']}
                     start={{ x: 0.0, y: 1.0 }}
                     end={{ x: 1.0, y: 1.0 }}
                     style={styles.storyGradient}
                   >
                     <Image
-                      resizeMode="cover"
+                      resizeMode='cover'
                       style={{
                         width: 58,
                         height: 58,
                         borderRadius: 58 / 2,
-                        alignSelf: "center",
-                        borderColor: "#fff",
+                        alignSelf: 'center',
+                        borderColor: '#fff',
                         borderWidth: 2,
                       }}
-                      source={require("./src/assets/images/profile-4.jpg")}
+                      source={require('./src/assets/images/profile-4.jpg')}
                     />
                   </LinearGradient>
                   <Text style={styles.storyText}>craig_love</Text>
                 </View>
                 <View style={styles.story}>
                   <LinearGradient
-                    colors={["#CA1D7E", "#E35157", "#F2703F"]}
+                    colors={['#CA1D7E', '#E35157', '#F2703F']}
                     start={{ x: 0.0, y: 1.0 }}
                     end={{ x: 1.0, y: 1.0 }}
                     style={styles.storyGradient}
                   >
                     <Image
-                      resizeMode="cover"
+                      resizeMode='cover'
                       style={{
                         width: 58,
                         height: 58,
                         borderRadius: 58 / 2,
-                        alignSelf: "center",
-                        borderColor: "#fff",
+                        alignSelf: 'center',
+                        borderColor: '#fff',
                         borderWidth: 2,
                       }}
-                      source={require("./src/assets/images/profile-5.jpg")}
+                      source={require('./src/assets/images/profile-5.jpg')}
                     />
                   </LinearGradient>
 
@@ -165,20 +165,20 @@ export default function App() {
           <View style={styles.postHeader}>
             <View style={styles.postUserInfo}>
               <Image
-                resizeMode="cover"
+                resizeMode='cover'
                 style={{
                   width: 40,
                   height: 40,
                   borderRadius: 500,
                 }}
-                source={require("./src/assets/images/profile-2.jpg")}
+                source={require('./src/assets/images/profile-2.jpg')}
               />
               <View style={styles.postUserName}>
                 <View style={styles.postVerfied}>
                   <Text
                     style={{
                       fontSize: 14,
-                      fontWeight: "bold",
+                      fontWeight: 'bold',
                       paddingRight: 4,
                     }}
                   >
@@ -193,9 +193,9 @@ export default function App() {
           </View>
           <View style={styles.postImage}>
             <Image
-              style={{ width: "100%", height: "100%" }}
-              resizeMode="cover"
-              source={require("./src/assets/images/post_image.jpg")}
+              style={{ width: '100%', height: '100%' }}
+              resizeMode='cover'
+              source={require('./src/assets/images/post_image.jpg')}
             />
           </View>
           <View style={styles.postReactionContainer}>
@@ -210,15 +210,32 @@ export default function App() {
                 <MessengerIcon />
               </View>
             </View>
-            <View style={{ position: "absolute", top: 0, right: 0 }}>
+            <View style={{ position: 'absolute', top: 0, right: 10 }}>
               <SaveIcon />
             </View>
-            <View>
+            <View style={{ position: 'absolute', top: 10 }}>
               <PaginationIcon />
             </View>
           </View>
+          <View style={styles.postLike}>
+            <View style={styles.postPersonThumbnail}>
+              <Image  style={{ width: 21, height: 21, borderRadius: 500}} source={require('./src/assets/images/profile-4.jpg')}/>
+            </View>
+            <View >
+              <Paragraph>Liked by 
+                <Text style={{ fontWeight: 'bold'}}> craig_love </Text>
+                 and  
+                <Text style={{ fontWeight: 'bold'}}> 44,686 others </Text>
+              </Paragraph> 
+            </View>
+          </View>
+          <View style={styles.postDescription}>
+            <Paragraph style={{ fontSize: 12 }}>
+            <Text style={{ fontSize: 14, fontWeight: 'bold'}}>karennne </Text>
+             The game in New York was amazing and I want to share some photos</Paragraph>
+          </View>
         </View>
-        <StatusBar style="auto" />
+        <StatusBar style='auto' />
       </View>
     </Provider>
   );
@@ -227,61 +244,61 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignContent: "center",
-    justifyContent: "flex-start",
+    backgroundColor: '#fff',
+    alignContent: 'center',
+    justifyContent: 'flex-start',
   },
   header: {
-    backgroundColor: "#FAFAFA",
+    backgroundColor: '#FAFAFA',
     height: 60,
-    justifyContent: "center",
-    alignContent: "center",
+    justifyContent: 'center',
+    alignContent: 'center',
     borderBottomWidth: 0.6,
-    borderBottomColor: "#DDD",
+    borderBottomColor: '#DDD',
   },
   row: {
-    flexDirection: "row",
-    position: "relative",
-    justifyContent: "center",
-    alignContent: "center",
+    flexDirection: 'row',
+    position: 'relative',
+    justifyContent: 'center',
+    alignContent: 'center',
   },
   cameraIcon: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 10,
   },
   igtvIcon: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     right: 10,
     marginRight: 40,
   },
   messengerIcon: {
-    position: "absolute",
+    position: 'absolute',
     top: 2,
     right: 10,
   },
   ovalIcon: {
-    position: "absolute",
+    position: 'absolute',
     top: 2,
     right: 49,
   },
   stories: {
-    flexDirection: "row",
-    alignItems: "flex-start",
+    flexDirection: 'row',
+    alignItems: 'flex-start',
     marginVertical: 10,
   },
   story: {
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     marginHorizontal: 10,
   },
   storyGradient: {
-    position: "relative",
+    position: 'relative',
     height: 62,
     width: 62,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 62 / 2,
     marginBottom: 5,
   },
@@ -290,67 +307,83 @@ const styles = StyleSheet.create({
   },
 
   storyLive: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 14,
     paddingHorizontal: 4,
     paddingVertical: 2,
     borderWidth: 2,
-    borderColor: "white",
+    borderColor: 'white',
     borderRadius: 5,
   },
   storyLiveText: {
-    color: "white",
+    color: 'white',
     fontSize: 8,
   },
   postContainer: {
     marginVertical: 10,
-    borderTopColor: "#DDD",
+    borderTopColor: '#DDD',
     borderTopWidth: 0.6,
   },
   postHeader: {
-    position: "relative",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
+    position: 'relative',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
     paddingVertical: 8,
     paddingHorizontal: 10,
   },
   postUserInfo: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   postUserName: {
     paddingHorizontal: 10,
   },
   postVerfied: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "flex-start",
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
 
   postHeadermoreIcon: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 10,
   },
   postImage: {
-    width: "100%",
-    height: 300,
+    width: '100%',
+    height: 250,
   },
   postReactionContainer: {
-
-
-    justifyContent: "center",
-    alignItems: "center",
-    marginVertical: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 5,
   },
   postReaction: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
+  postLike: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    marginTop: 26
+  },
+  postPersonThumbnail: {
+    width: 24,
+    height: 24,
+    borderRadius: 500,
+    marginRight: 2
+  },
+  postDescription: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 10,
+
+  }
 });
