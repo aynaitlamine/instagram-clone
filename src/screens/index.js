feb95d98ad21ddd84f217ee911a2c61418b56337
Ayoub Nait Lamine <aynaitlamine.dev@gmail.com>
2022-03-28 10:04:10 +0000

    Add navigation

---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+export { default as HomeScreen} from './HomeScreen'
+export { default as SearchScreen} from './SearchScreen'
+export { default as AddScreen} from './AddScreen'
+export { default as LikeScreen} from './LikeScreen'
+export { default as ProfileScreen} from './ProfileScreen'
